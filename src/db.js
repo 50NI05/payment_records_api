397b4e14d84e79bb83341e3bc29cbdcf4a93a6cb
jonathan836 <jonathan999aaa@gmail.com>
2023-07-22 10:32:00 -0400

    database connection

--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
 import Sequelize from "sequelize";
 import { UserModel } from "../src/models/User.js";
 import { PaymentModel } from "../src/models/Payment.js"
-const sequelize = new Sequelize('mysql://root:123456@localhost:3306/registerPaymentDB')
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from './config.js'
+const sequelize = new Sequelize(`mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`);
 export const User = UserModel(sequelize);
 export const Payment = PaymentModel(sequelize)
