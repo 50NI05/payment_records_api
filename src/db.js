6013c013d6bc4cf386ebdaf72f95417dba8f362e
simon.saad <simon.saad@tcs.com.ve>
2023-08-07 13:24:35 -0400

    connection db

--- src/db.js
+++ src/db.js
@@ -3,6 +3,6 @@ import { UserModel } from "../src/models/User.js";
 import { PaymentModel } from "../src/models/Payment.js"
 import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from './config.js'
 // const sequelize = new Sequelize(`mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`);
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`)
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?sslmode=require`)
 export const User = UserModel(sequelize);
 export const Payment = PaymentModel(sequelize)
